3df73f0bfbddf4edf276228098f75afbb94b284a
giuxtaposition <yg97.cs@gmail.com>
2021-10-06 09:40:06 +0200

    saved volumeCreditsFor result in statementData

--- app/index.ts
+++ app/index.ts
@@ -12,6 +12,7 @@ export function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
   function playFor(aPerformance) {
@@ -39,6 +40,15 @@ export function statement(invoice, plays) {
     }
     return result;
   }
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    // add extra credit for every ten comedy attendees
+    if ("comedy" === aPerformance.play.type) {
+      result += Math.floor(aPerformance.audience / 5);
+    }
+    return result;
+  }
 }
 
 function renderPlainText(data) {
@@ -53,16 +63,6 @@ function renderPlainText(data) {
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === aPerformance.play.type) {
-      result += Math.floor(aPerformance.audience / 5);
-    }
-    return result;
-  }
-
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
@@ -74,7 +74,7 @@ function renderPlainText(data) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
